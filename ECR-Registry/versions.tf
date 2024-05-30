e7f634410a25d08f3a69d530b29f25118957380f
Jason B <jason.nzd@gmail.com>
2024-05-30 13:41:13 +1200

    updated providers

--- ECR-Registry/versions.tf
+++ ECR-Registry/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.4.2"
+  required_version = ">= 1.8.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.3.0"
+      version = "~> 5.50.0"
     }
   }
 }
--- ECS-Service/versions.tf
+++ ECS-Service/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.4.2"
+  required_version = ">= 1.8.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.3.0"
+      version = "~> 5.50.0"
     }
     http = {
       source  = "hashicorp/http"
-      version = "~> 3.2.1"
+      version = "~> 3.4.0"
     }
   }
 }
